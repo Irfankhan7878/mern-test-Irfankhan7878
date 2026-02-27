cf148117c5b12ad9063b1f8a95991766f53273da
Irfan khan <ikan13545@gmail.com>
2026-02-27 14:58:45 +0530

    Add Student and Course mongoose models

---
+++ backend/config/src/models/Course.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+  courseName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  courseDescription: {
+    type: String,
+    required: true,
+  },
+  instructor: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Course', courseSchema);
